f9ebaf19b2d6640a76673eafd5b9474f56fd810c
Greg L. Turnquist <gturnquist@vmware.com>
2021-02-13 23:26:13 -0600

    Polish

--- src/main/java/com/example/demo/StockWatcher.java
+++ src/main/java/com/example/demo/StockWatcher.java
@@ -1,15 +1,16 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class StockWatcher {
@@ -21,8 +22,8 @@ public class StockWatcher {
 	@RabbitListener( //
 			bindings = @QueueBinding( //
 					value = @Queue, //
-					exchange = @Exchange("stock-market"), //
-					key = "SBUX"))
+					exchange = @Exchange(value = "stock-market", type = ExchangeTypes.TOPIC), //
+					key = "*"))
 	void listenForStockTrades(StockMovement trade) {
 		logger.info("Received an update on " + trade.getStockName() + " => NEW PRICE: " + trade.getPrice());
 		trades.add(trade); // track all trades for testing
